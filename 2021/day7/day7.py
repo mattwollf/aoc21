6050d2aede617e951b46c3c45df8ead54aaad059
Matt Wolf <mattwollf@gmail.com>
2021-12-08 10:23:40 -0500

    day 8

---
+++ 2021/day7/day7.py
@@ -0,0 +1,119 @@
+import collections
+
+
+def next_day(d: dict[int:int]):
+    ret = collections.defaultdict(lambda: 0)
+
+    for (k, v) in d.items():
+        if k == 0:
+            ret[8] = v
+            ret[6] = ret[6] + v
+        else:
+            ret[k-1] = ret[k-1] + v
+    return ret
+
+
+def next_day_generator(d: dict[int:int]):
+    ret = collections.defaultdict(lambda: 0)
+
+    ret[8] = d[0]
+    ret[6] = ret[6] + d[0]
+
+    for (k, v) in (x for x in d.items() if x[0] != 0):
+        ret[k-1] = ret[k-1] + v
+    return ret
+
+
+def next_day_arr(fish):
+    reset = fish[0]
+    fish[0] = 0
+    for c in range(1, 9):
+        fish[c-1] = fish[c]
+
+    fish[8] = reset
+    fish[6] = fish[6] + reset
+    return fish
+
+
+def part1_arr():
+    f = open("input.txt", "r")
+
+    nums = [int(c) for c in [x.rstrip() for x in f][0].split(',')]
+
+    fish = [0] * 9
+
+    for days_left in nums:
+        fish[days_left] += 1
+
+    for _ in range(80):
+        next_day_arr(fish)
+    return sum(fish)
+
+
+def part2_arr(init):
+    fish_buckets = [0] * 9
+
+    for i in init:
+        fish_buckets[i] += 1
+
+    for _ in range(256):
+        births = fish_buckets[0]
+
+        fish_buckets = fish_buckets[1:9] + [births]
+        fish_buckets[6] = fish_buckets[6] + births
+
+    return sum(fish_buckets)
+
+
+def part1():
+    f = open("input.txt", "r")
+
+    nums = [int(c) for c in [x.rstrip() for x in f][0].split(',')]
+
+    num_count = collections.defaultdict(lambda: 0)
+    for n in nums:
+        num_count[n] = num_count[n] + 1
+
+    for i in range(80):
+        num_count = next_day(num_count)
+
+    return sum(num_count.values())
+
+
+def part2(nums):
+    num_count = collections.defaultdict(lambda: 0)
+    for n in nums:
+        num_count[n] = num_count[n] + 1
+
+    for _ in range(256):
+        # num_count = next_day_generator(num_count)
+        num_count = next_day(num_count)
+
+    return sum(num_count.values())
+
+
+def count(f, n=10000):
+    import time
+    start = time.time()
+    for _ in range(n):
+        f()
+    end = time.time()
+    print((end-start) / n)
+
+
+if __name__ == '__main__':
+    from ryans import ryan_part_2
+
+    f = open("input.txt", "r")
+
+    initial = [int(c) for c in [x for x in f][0].split(',')]
+    f.close()
+    print(part2(initial))
+    print(part2_arr(initial))
+    print(ryan_part_2(initial))
+
+    rerun = 10000
+    count(lambda: part2(initial))
+    count(lambda: part2_arr(initial), rerun)
+    count(lambda: ryan_part_2(initial), rerun)
+
---
+++ 2021/day8/day8.py
@@ -0,0 +1,109 @@
+def part1(lines):
+    lines = unique_count_replacement(lines)
+
+    result = 0
+    for sides in lines:
+        rhs = sides[1]
+        rhs = map(lambda x: 1,
+                  filter(lambda x: type(x) == int, rhs)
+                  )
+        result += sum(rhs)
+    return result
+
+
+def unique_count_replacement(lines):
+    for sides in lines:
+        for s in sides:
+            for i in range(len(s)):
+                if   len(s[i]) == 2 and type(s[i]) == str: s[i] = 1
+                elif len(s[i]) == 3:                       s[i] = 7
+                elif len(s[i]) == 4:                       s[i] = 4
+                elif len(s[i]) == 7:                       s[i] = 8
+    return lines
+
+
+def find_sig(line, number):
+
+    len_to_num = {
+        2: 1,
+        3: 7,
+        4: 4,
+        7: 8
+    }
+
+    return next(e for e in line if len(e) in len_to_num.keys() and len_to_num[len(e)] == number)
+
+
+def part2(lines):
+    result = 0
+    for count, line in enumerate(lines):
+
+        lhs, rhs = line
+
+        sig1 = find_sig(lhs, 1)
+        sig4 = find_sig(lhs, 4)
+        sig7 = find_sig(lhs, 7)
+        sig8 = find_sig(lhs, 8)
+
+        zero_six_nine = set([e for e in lhs if len(e) == 6])
+        sig6 = next(x for x in zero_six_nine if len(set(sig1).difference(set(x))) == 1)
+        zero_nine = zero_six_nine.difference({''.join(sorted(sig6))})
+        letter_top_right = next(iter(set(sig1).difference(set(sig6))))
+        two_three_five = set([e for e in lhs if len(e) == 5])
+        four_without_top_right = set(sig4).difference(set(letter_top_right))
+        sig5 = next(
+            x for x in two_three_five if set(x).intersection(four_without_top_right) == four_without_top_right
+        )
+        letter_bottom_right = next(iter(set(sig1).difference(set(letter_top_right))))
+        sig2 = next(x for x in two_three_five if letter_bottom_right not in x)
+        sig3 = next(iter(two_three_five.difference({''.join(sorted(sig5)), ''.join(sorted(sig2))})))
+        sig9 = next(x for x in zero_nine if set(x).intersection(four_without_top_right) == four_without_top_right)
+        sig0 = zero_nine.difference({sig9})
+
+        mapping = {
+            ''.join(sorted(sig1)): 1,
+            ''.join(sorted(sig2)): 2,
+            ''.join(sorted(sig3)): 3,
+            ''.join(sorted(sig4)): 4,
+            ''.join(sorted(sig5)): 5,
+            ''.join(sorted(sig6)): 6,
+            ''.join(sorted(sig7)): 7,
+            ''.join(sorted(sig8)): 8,
+            ''.join(sorted(sig9)): 9,
+            ''.join(sorted(sig0)): 0,
+        }
+
+        digits = [str(mapping[x]) for x in rhs]
+        result += int(''.join(digits))
+    return result
+
+
+def tests():
+    lines = parse_input('sample.txt')
+    assert part1(lines) == 26
+    lines = parse_input('sample.txt')
+    assert part2(lines) == 61229
+
+
+def parse_input(name):
+    with open(name, 'r') as f:
+
+        result = []
+        for line in f.readlines():
+            lhs, rhs = line.split(' | ')
+            lhs = lhs.split(' ')
+            rhs = rhs.rstrip().split(' ')
+            lhs = [''.join(sorted(list(x))) for x in lhs]
+            rhs = [''.join(sorted(list(x))) for x in rhs]
+            result.append([lhs, rhs])
+        return result
+
+
+if __name__ == '__main__':
+    i = parse_input('input.txt')
+    tests()
+    print(part1(i))
+    i2 = parse_input('input.txt')
+    print(part2(i2))
+
+    # print(part2())
